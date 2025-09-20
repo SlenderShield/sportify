fe34bd4c1b1e4fdd9ec37f27cf28133c50105727
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2025-09-20 18:06:10 +0530

    init project

--- app/dashboard/index.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardScreen from "@features/dashboard/DashboardScreen";
-
-export default function Dashboard() {
-  return <DashboardScreen />;
-}
--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-// TODO: Replace with auth state logic
-const isAuthenticated = true;
-
-export default function Index() {
-  return <Redirect href={isAuthenticated ? '/dashboard' : '/auth/login'} />;
-}
